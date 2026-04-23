04376c1da17fbcaaf080ef0d684a7647786ea987
KristoferTooding1 <tooding2006@gmail.com>
2026-04-23 19:58:01 +0300

    Router add, improved search function

---
+++ frontend/src/pages/Favourites.jsx
@@ -0,0 +1,8 @@
+function Favourite() {
+    return <div className="favourites-empty">
+        <h2>Your favourites list is empty</h2>
+        <p>Start adding movies to your favourites list by clicking the heart icon on movie cards.</p>
+    </div>
+}
+
+export default Favourite;
--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,25 +1,32 @@
 import MovieCard from "../components/MovieCard";
+import { useState } from "react";
 
 function Home() {
+    const [searchQuery, setSearchQuery] = useState("");
+
     const movies = [
         { id: 1, title: "Terminator", release_date: "1984-10-26" },
         { id: 2, title: "John Wick", release_date: "1984-10-26" },
         { id: 3, title: "Titanic", release_date: "1984-10-26" },
     ];
 
-    const handleSearch = () => {
-
+    const handleSearch = (e) => {
+        e.preventDefault();
+        alert(searchQuery);
+        setSearchQuery("");
     }
 
 
     return (
         <div className="home">
             <form onSubmit={handleSearch} className="search-form">
-                <input type="text" placeholder="Search for movies..." className="search-input" />
+                <input type="text" placeholder="Search for movies..." 
+                className="search-input" value={searchQuery}
+                onChange={(e) => setSearchQuery(e.target.value)} />
                 <button type="submit" className="search-button">Search</button>
             </form>
             <div className="movies-grid">
-                {movies.map(movie => (
+                {movies.map((movie) => (
                     <MovieCard movie={movie} key={movie.id} />
                 ))}
             </div>
