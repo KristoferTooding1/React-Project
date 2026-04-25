7824b9a9404895e8e1a72210398959a084534070
KristoferTooding1 <tooding2006@gmail.com>
2026-04-25 14:16:18 +0300

    API file

---
+++ frontend/src/services/api.js
@@ -0,0 +1,8 @@
+const API_KEY = "8e39b36a73fba656b58b33bb5a5bc98d";
+const BASE_URL = "https://api.themoviedb.org/3";
+
+export const getPopularMovies = async () => {
+    const response = await fetch(`${BASE_URL}/movie/popular?api_key=${API_KEY}`);
+    const data = await response.json();
+    return data.results;
+};
