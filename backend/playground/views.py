268d80dd334aa42054d286ebbc3053eb27d46d18
KristoferTooding1 <tooding2006@gmail.com>
2026-04-30 20:00:09 +0300

    debugger

--- backend/playground/views.py
+++ backend/playground/views.py
@@ -2,4 +2,4 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def say_hello(request):
-    return HttpResponse("Hello, world! This is the playground app.")
+    return render(request, "start.html", { "name": "Django" })
